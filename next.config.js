36ba091e55643319d86117140ed2f30b1d4bccf0
Amr Ahmed <amr@intouch.com>
2024-09-01 16:21:50 +0300

    fix: warning font.sanSerif changed in server-side and client hydration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    // to avoid bug where using styled-components with server-side rendering (font.sansSerif was throwing Warning)
+    styledComponents: true
+  },
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     AUTH0_BASE_URL: process.env.VERCEL_URL || process.env.AUTH0_BASE_URL,
