4b762f52d41d9afb886de4f2f1d58c75ebf750dd
Amr Ahmed <amr@intouch.com>
2024-10-01 03:49:25 +0300

    remove: auth0 & mui

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import { ThemeProvider } from '@/components/theme-provider';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from '@clerk/themes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useTheme } from 'next-themes';
 import React from 'react';
 
 interface IProps {
@@ -24,13 +21,11 @@ const queryClient = new QueryClient({
 
 const Providers = ({ children }: IProps) => {
   return (
-    <UserProvider>
-      <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-        <ClerkProvider>
-          <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-        </ClerkProvider>
-      </ThemeProvider>
-    </UserProvider>
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+      <ClerkProvider>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </ClerkProvider>
+    </ThemeProvider>
   );
 };
 
